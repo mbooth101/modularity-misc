f0c480d526460827b1ab37a69a11f4660d70dc51
Mat Booth <mat.booth@redhat.com>
2019-11-21 10:04:24 +0000

    Improved cache module script

---
+++ cache_module.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+module_nom=${1:?Specify a module name}
+module_str=${2:?Specify a module stream}
+module_fed=${3:?Specify a Fedora version}
+
+module="$module_nom-$module_str-$module_fed"
+
+echo "Finding latest tag for module: $module"
+tag="$(koji list-tags | grep "${module}" | grep -v '\-build$' | sort | tail -n1)"
+if [ -z "$tag" ] ; then
+	echo "No tag found"
+	exit 1
+fi
+tag_prefix=module-$module_nom-$module_str-
+module_ver=$(echo ${tag#$tag_prefix} | sed -e 's/-/./')
+
+echo "Caching builds from tag: $tag"
+if [ ! -f "module-cache/$module.cache" ] ; then
+	mkdir -p module-cache/$module
+	pushd module-cache/$module 2>&1 >/dev/null
+	pkgs=$(koji list-pkgs --quiet --tag=$tag | cut -f1 -d' ' | sort)
+	for pkg in $pkgs ; do
+		if [ "$pkg" = "module-build-macros" ] ; then
+			continue
+		fi
+		koji download-build --arch=noarch --arch=x86_64 --latestfrom=$tag $pkg
+	done
+	popd 2>&1 >/dev/null
+	wget https://kojipkgs.fedoraproject.org//vol/fedora_koji_archive02/packages/$module_nom/$module_str/$module_ver/files/module/modulemd.x86_64.txt -O module-cache/$module-modulemd.txt
+	touch module-cache/$module.cache
+fi
+
+# Create yum repo
+createrepo_c module-cache/$module
+modifyrepo_c --mdtype=modules module-cache/$module-modulemd.txt module-cache/$module/repodata
+
+# Generate repo file
+cat <<EOF > module-cache/$module.repo
+[$module]
+name=$module
+baseurl=file://$(pwd)/module-cache/$module
+enabled=1
+sslverify=0
+gpgcheck=0
+priority=5
+EOF
+echo "Done caching builds from tag: $tag"
